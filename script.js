ad39669f50a5d4ce166e27811b254757c97738da
bolot6 <kurmanaliev78@gmail.com>
2023-11-03 16:01:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//Homework
+
+//Task1
+// делать каунтер, добавить умножение, сделать возможность выбора цифры для умножения.
+// const btnSubmit = document.querySelector('#btn-submit');
+// const input1= document.querySelector('#num1');
+// const input2 = document.querySelector('#num2');
+//
+// btnSubmit.addEventListener('click', () => {
+//     const sum = Number(input1.value) * Number(input2.value);
+//     alert(sum)
+// })
+
+//task2
+//сделать свой алерт с кнопкой закрытия
+// const btnSubmit = document.querySelector('#btn-submit');
+// const input1= document.querySelector('#num1');
+// const input2 = document.querySelector('#num2');
+//
+// btnSubmit.addEventListener('click', () => {
+//     const num1 = Number(input1.value);
+//     const num2 = Number(input2.value);
+//
+//     if (num1 < 0 || num2 < 0) {
+//         alert('Cannot multiply negative numbers');
+//     } else {
+//         const sum = num1 * num2;
+//         alert(sum);
+//     }
+// })
+
+//В этом задании если ввести в одну из окошек отрицательное число, выйдет алерт с сообщением 'Cannot multiply negative numbers'. Если число положительное, то он сделает умножение
+
+
